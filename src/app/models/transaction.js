c4df0f4f6332925e601d5d6dc0b2b0e42b57e5fd
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-08-22 10:41:24 -0300

    Rewrites Cashier model

--- src/app/models/transaction.js
+++ src/app/models/transaction.js
@@ -1,13 +1,24 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Transaction = sequelize.define('Transaction', {
-    type: DataTypes.INTEGER,
-    value: DataTypes.FLOAT,
-    category: DataTypes.INTEGER,
-    description: DataTypes.TEXT
-  }, {});
-  Transaction.associate = function(models) {
-    // associations can be defined here
-  };
-  return Transaction;
-};
+import Sequelize, { Model } from 'sequelize';
+
+class Transaction extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        type: Sequelize.INTEGER,
+        value: Sequelize.FLOAT,
+        category: Sequelize.INTEGER,
+        description: Sequelize.TEXT,
+      },
+      { sequelize }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Cashier, { foreignKey: 'cashierId', as: 'cashier' });
+  }
+}
+
+export default Transaction;
+
