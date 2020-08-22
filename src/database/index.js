0845ed1213d4d67fed068745e3de17ca578acdbe
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-08-22 09:34:39 -0300

    Sequelize setup

--- src/database/index.js
+++ src/database/index.js
@@ -16,7 +16,6 @@ class Database {
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
   }
-
 }
 
 export default new Database();
