119b5036604c25e37aa6c87b0d304aeeab0ab2d2
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-08-23 18:25:05 -0300

    Fix port for heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from './app';
 
 const port = process.env.NODE_ENV === 'development' ? 3333 : 3339;
 
-const server = app.listen(port);
+const server = app.listen(process.env.PORT || port);
 
 export const stop = () => server.close();
 
