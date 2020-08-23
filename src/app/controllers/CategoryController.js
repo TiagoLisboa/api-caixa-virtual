de0008209a8fc80f00efc78f13802cc1b2c42415
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-08-23 15:33:08 -0300

    Creates category update method

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -61,6 +61,42 @@ class CategoryController {
         .send({ message: 'error while creating category.' });
     }
   }
+
+  /**
+   * updates a instance of the resource.
+   *
+   * @param {Request}  req
+   * @param {Response} res
+   *
+   * @return {void}
+   */
+  async update(req, res) {
+    const id = req.params.categoryId;
+    const categorySchema = Joi.object({
+      name: Joi.string().required(),
+    });
+    let result;
+    try {
+      result = validateSchema(req.body, categorySchema);
+    } catch (e) {
+      if (e instanceof ValidationException) {
+        return res.status(422).send(e);
+      }
+    }
+    const category = await Category.findByPk(id);
+    if (!category) {
+      return res.status(404).send({ message: 'Not found' });
+    }
+    try {
+      const { name } = result;
+      await category.update({ name });
+      return res.send(new CategoryResourcer(category));
+    } catch (err) {
+      return res
+        .status(400)
+        .send({ message: 'error while updating category.' });
+    }
+  }
 }
 
 export default new CategoryController();
